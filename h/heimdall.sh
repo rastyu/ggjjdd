4ab871c10ffe799e8fb489d2cefabbc8f4766201
rastyu <48859414+rastyu@users.noreply.github.com>
2023-01-27 08:53:36 +0800

    Update heimdall.sh

--- h/heimdall.sh
+++ h/heimdall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "docker一键脚本，heimdall改变端口"
 echo "-------------------端口文件替换-------------------"
-rm -f /
+rm -f /config/nginx/site-confs/default.conf
 wget -P / 
 rm -f /
 wget -P /168.10.5:5300/d/%E6%9C%AC%E5%9C%B0%E5%AD%98%E5%82%A8/4.7.11/light/theme.css
