c31ec094b1a53d3b9ab9d5bc105e65cbc85432ae
rastyu <48859414+rastyu@users.noreply.github.com>
2023-02-12 21:26:53 +0800

    Update npm.sh

--- h/npm.sh
+++ h/npm.sh
@@ -2,15 +2,15 @@
 echo "docker一键脚本，npm改变端口"
 echo "-------------------端口文件替换-------------------"
 rm -f /etc/nginx/conf.d/default.conf
-curl -fsSL /etc/nginx/conf.d/ https://ghproxy.com/https://raw.githubusercontent.com/rastyu/ggjjdd/main/npm/default.conf
+curl -o /etc/nginx/conf.d/default.conf https://ghproxy.com/https://raw.githubusercontent.com/rastyu/ggjjdd/main/npm/default.conf
 rm -f /etc/nginx/conf.d/production.conf
-curl -fsSL /etc/nginx/conf.d/ https://ghproxy.com/https://raw.githubusercontent.com/rastyu/ggjjdd/main/npm/production.conf
+curl -o /etc/nginx/conf.d/production.conf https://ghproxy.com/https://raw.githubusercontent.com/rastyu/ggjjdd/main/npm/production.conf
 rm -f /app/templates/_listen.conf
-curl -fsSL /app/templates/ https://ghproxy.com/https://raw.githubusercontent.com/rastyu/ggjjdd/main/npm/templates/_listen.conf
+curl -o /app/templates/_listen.conf https://ghproxy.com/https://raw.githubusercontent.com/rastyu/ggjjdd/main/npm/templates/_listen.conf
 rm -f /app/templates/default.conf
-curl -fsSL /app/templates/ https://ghproxy.com/https://raw.githubusercontent.com/rastyu/ggjjdd/main/npm/templates/default.conf
+curl -o /app/templates/default.conf https://ghproxy.com/https://raw.githubusercontent.com/rastyu/ggjjdd/main/npm/templates/default.conf
 rm -f /app/templates/letsencrypt-request.conf
-curl -fsSL /app/templates/ https://ghproxy.com/https://raw.githubusercontent.com/rastyu/ggjjdd/main/npm/templates/letsencrypt-request.conf
+curl -o /app/templates/letsencrypt-request.conf https://ghproxy.com/https://raw.githubusercontent.com/rastyu/ggjjdd/main/npm/templates/letsencrypt-request.conf
 echo "替换端口完成..."
 rm -rf npm.sh
 echo "-------------------删除脚本完成，请重启容器-------------------"
