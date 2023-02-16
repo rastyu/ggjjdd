bcbf2904524c2879c811c8e87ed96a4840c60fff
rastyu <48859414+rastyu@users.noreply.github.com>
2023-02-16 17:55:40 +0800

    Update heimdall.sh

--- h/heimdall.sh
+++ h/heimdall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "docker一键脚本，heimdall改变端口"
 echo "-------------------端口文件替换-------------------"
-# rm -f /config/nginx/site-confs/default.conf
+rm -f /root/defaults/nginx/site-confs/default.conf.sample
 wget -P /root/defaults/nginx/site-confs/ https://raw.githubusercontent.com/rastyu/ggjjdd/main/heimdall/default.conf
 rm -f /etc/php81/php-fpm.d/www.conf
 wget -P /etc/php81/php-fpm.d/ https://raw.githubusercontent.com/rastyu/ggjjdd/main/heimdall/www.conf
