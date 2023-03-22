66a18e98d5208472e5fa70b68b44b2e846efccf5
rastyu <48859414+rastyu@users.noreply.github.com>
2023-03-22 09:39:22 +0800

    Update npm.sh

--- h/npm.sh
+++ h/npm.sh
@@ -2,16 +2,16 @@
 echo "docker一键脚本，npm改变端口"
 echo "-------------------端口文件替换-------------------"
 rm -f /etc/nginx/conf.d/default.conf
-curl -o /etc/nginx/conf.d/default.conf https://ghproxy.com/https://raw.githubusercontent.com/rastyu/ggjjdd/main/npm/default.conf
+curl -o /etc/nginx/conf.d/default.conf https://raw.githubusercontent.com/rastyu/ggjjdd/main/npm/default.conf
 rm -f /etc/nginx/conf.d/production.conf
-curl -o /etc/nginx/conf.d/production.conf https://ghproxy.com/https://raw.githubusercontent.com/rastyu/ggjjdd/main/npm/production.conf
+curl -o /etc/nginx/conf.d/production.conf https://raw.githubusercontent.com/rastyu/ggjjdd/main/npm/production.conf
 rm -f /app/templates/_listen.conf
-curl -o /app/templates/_listen.conf https://ghproxy.com/https://raw.githubusercontent.com/rastyu/ggjjdd/main/npm/templates/_listen.conf
+curl -o /app/templates/_listen.conf https://raw.githubusercontent.com/rastyu/ggjjdd/main/npm/templates/_listen.conf
 rm -f /app/templates/default.conf
-curl -o /app/templates/default.conf https://ghproxy.com/https://raw.githubusercontent.com/rastyu/ggjjdd/main/npm/templates/default.conf
+curl -o /app/templates/default.conf https://raw.githubusercontent.com/rastyu/ggjjdd/main/npm/templates/default.conf
 rm -f /app/templates/letsencrypt-request.conf
-curl -o /app/templates/letsencrypt-request.conf https://ghproxy.com/https://raw.githubusercontent.com/rastyu/ggjjdd/main/npm/templates/letsencrypt-request.conf
+curl -o /app/templates/letsencrypt-request.conf https://raw.githubusercontent.com/rastyu/ggjjdd/main/npm/templates/letsencrypt-request.conf
 echo "替换端口完成..."
-cd /usr/local/lib/python3.7 && python3 -m venv tutorial-env && source tutorial-env/bin/activate && pip install certbot-dns-dnspod && pip install zope
+pip install certbot-dns-dnspod && pip install zope
 rm -rf npm.sh
 echo "-------------------删除脚本完成，请重启容器-------------------"
