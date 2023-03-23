29648c796409e7f20b392421628d2a561273caec
rastyu <48859414+rastyu@users.noreply.github.com>
2023-03-23 12:20:16 +0800

    Update test.sh

--- h/test.sh
+++ h/test.sh
@@ -1,3 +1,3 @@
-pip install --upgrade setuptools && python3 -m pip install --upgrade pip && pip install certbot-dns-dnspod && pip install zope
+pip install --upgrade setuptools && python3 -m pip install --upgrade pip && pip install persistent && pip install certbot-dns-dnspod && pip install zope
 rm -rf test.sh
 echo "-------------------删除脚本完成，请重启容器-------------------"
