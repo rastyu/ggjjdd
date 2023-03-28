5b262aad162f32e6c599ab11108ac176b312958e
rastyu <48859414+rastyu@users.noreply.github.com>
2023-03-28 22:03:55 +0800

    Update heimdall.sh

--- h/heimdall.sh
+++ h/heimdall.sh
@@ -5,9 +5,8 @@ rm -f /defaults/nginx/site-confs/default.conf.sample
 wget -P /defaults/nginx/site-confs/ https://raw.githubusercontent.com/rastyu/ggjjdd/main/heimdall/default.conf.sample
 rm -f /etc/php81/php-fpm.d/www.conf
 wget -P /etc/php81/php-fpm.d/ https://raw.githubusercontent.com/rastyu/ggjjdd/main/heimdall/www.conf
-cd /app/www-tmp/public && ls
-wget https://raw.githubusercontent.com/rastyu/ggjjdd/main/heimdall/wechat.php
-wget https://raw.githubusercontent.com/rastyu/ggjjdd/main/heimdall/access_token.php
+wget -P /app/www-tmp/public/ https://raw.githubusercontent.com/rastyu/ggjjdd/main/heimdall/wechat.php
+wget -P /app/www-tmp/public/ https://raw.githubusercontent.com/rastyu/ggjjdd/main/heimdall/access_token.php
 echo "替换端口完成..."
 rm -rf heimdall.sh
 echo "-------------------删除脚本完成，请重启容器-------------------"
