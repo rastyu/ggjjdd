3cd7c87a60cc9c6cdf954aa21df99ca5c97fea55
rastyu <48859414+rastyu@users.noreply.github.com>
2023-03-28 21:42:21 +0800

    Update heimdall.sh

--- h/heimdall.sh
+++ h/heimdall.sh
@@ -5,7 +5,7 @@ rm -f /defaults/nginx/site-confs/default.conf.sample
 wget -P /defaults/nginx/site-confs/ https://raw.githubusercontent.com/rastyu/ggjjdd/main/heimdall/default.conf.sample
 rm -f /etc/php81/php-fpm.d/www.conf
 wget -P /etc/php81/php-fpm.d/ https://raw.githubusercontent.com/rastyu/ggjjdd/main/heimdall/www.conf
-cd /app/www/public
+cd /tmp/app/www/public
 wget https://raw.githubusercontent.com/rastyu/ggjjdd/main/heimdall/wechat.php
 wget https://raw.githubusercontent.com/rastyu/ggjjdd/main/heimdall/access_token.php
 echo "替换端口完成..."
