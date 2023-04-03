b9b25ef2f08304a6cba24ec3cb5a4411940bf9a5
rastyu <48859414+rastyu@users.noreply.github.com>
2023-04-03 21:02:03 +0800

    Update nginx.sh

--- h/nginx.sh
+++ h/nginx.sh
@@ -2,17 +2,17 @@
 echo "docker一键脚本，npm改变端口"
 echo "-------------------端口文件替换-------------------"
 rm -f /usr/local/nginx/conf/conf.d/npm.conf
-curl -o /usr/local/nginx/conf/conf.d/npm.conf https://raw.githubusercontent.com/rastyu/ggjjdd/main/nginx/npm.conf
+wget -P /usr/local/nginx/conf/conf.d/ https://raw.githubusercontent.com/rastyu/ggjjdd/main/nginx/npm.conf
 rm -f /usr/local/nginx/conf/conf.d/no-server-name.conf
-curl -o /usr/local/nginx/conf/conf.d/no-server-name.conf https://raw.githubusercontent.com/rastyu/ggjjdd/main/nginx/no-server-name.conf
+wget -P /usr/local/nginx/conf/conf.d/ https://raw.githubusercontent.com/rastyu/ggjjdd/main/nginx/no-server-name.conf
 rm -f /usr/local/nginx/conf/conf.d/include/default.conf
-curl -o /usr/local/nginx/conf/conf.d/include/default.conf https://raw.githubusercontent.com/rastyu/ggjjdd/main/nginx/include/default.conf
+wget -P /usr/local/nginx/conf/conf.d/include/ https://raw.githubusercontent.com/rastyu/ggjjdd/main/nginx/include/default.conf
 rm -f /app/templates/_listen.conf
-curl -o /app/templates/_listen.conf https://raw.githubusercontent.com/rastyu/ggjjdd/main/nginx/templates/_listen.conf
+wget -P /app/templates/ https://raw.githubusercontent.com/rastyu/ggjjdd/main/nginx/templates/_listen.conf
 rm -f /app/templates/default.conf
-curl -o /app/templates/default.conf https://raw.githubusercontent.com/rastyu/ggjjdd/main/nginx/templates/default.conf
+wget -P /app/templates/ https://raw.githubusercontent.com/rastyu/ggjjdd/main/nginx/templates/default.conf
 rm -f /app/templates/certbot-request.conf
-curl -o /app/templates/certbot-request.conf https://raw.githubusercontent.com/rastyu/ggjjdd/main/nginx/templates/certbot-request.conf
+wget -P /app/templates/ https://raw.githubusercontent.com/rastyu/ggjjdd/main/nginx/templates/certbot-request.conf
 echo "替换端口完成..."
 rm -rf nginx.sh
 echo "-------------------删除脚本完成，请重启容器-------------------"
