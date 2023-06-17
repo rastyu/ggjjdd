b45b387c4a2352db7514a979ca81070547aca8dc
rastyu <48859414+rastyu@users.noreply.github.com>
2023-06-17 13:48:57 +0800

    Update 4.7.13.sh

--- h/4.7.13.sh
+++ h/4.7.13.sh
@@ -2,17 +2,17 @@
 echo "docker一键脚本，适4.7.13版本"
 echo "-------------------web端-------------------"
 rm -f /system/Emby.Web.dll
-wget -P /system/ https://raw.githubusercontent.com/rastyu/ggjjdd/main/emby/4.7.13/Emby.Web.dll
+wget -P /system/ /rastyu/ggjjdd/main/emby/4.7.13/Emby.Web.dll
 rm -f /system/MediaBrowser.Model.dll
-wget -P /system/ https://raw.githubusercontent.com/rastyu/ggjjdd/main/emby/4.7.13/MediaBrowser.Model.dll
+wget -P /system/ /rastyu/ggjjdd/main/emby/4.7.13/MediaBrowser.Model.dll
 echo "核心替换完成..."
 rm -f /system/dashboard-ui/modules/emby-apiclient/connectionmanager.js
-wget -P /system/dashboard-ui/modules/emby-apiclient/ https://raw.githubusercontent.com/rastyu/ggjjdd/main/emby/4.7.13/connectionmanager.js
+wget -P /system/dashboard-ui/modules/emby-apiclient/ /rastyu/ggjjdd/main/emby/4.7.13/connectionmanager.js
 rm -f /system/dashboard-ui/embypremiere/embypremiere.js
-wget -P /system/dashboard-ui/embypremiere/ https://raw.githubusercontent.com/rastyu/ggjjdd/main/emby/4.7.13/embypremiere.js
+wget -P /system/dashboard-ui/embypremiere/ /rastyu/ggjjdd/main/emby/4.7.13/embypremiere.js
 echo "web替换完成..."
 rm -f /system/Emby.Server.Implementations.dll
-wget -P /system/ https://raw.githubusercontent.com/rastyu/ggjjdd/main/emby/4.7.13/Emby.Server.Implementations.dll
+wget -P /system/ /rastyu/ggjjdd/main/emby/4.7.13/Emby.Server.Implementations.dll
 echo "Implementations替换认证..."
 rm -rf 4.7.13.sh
 echo "-------------------删除脚本完成，预防重运行出错-------------------"
