ac0c079aa11039a360aa764d2374413a5f723273
rastyu <48859414+rastyu@users.noreply.github.com>
2023-06-17 13:10:37 +0800

    Update and rename 4.7.11.sh to 4.7.13.sh

---
+++ h/4.7.13.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "docker一键脚本，适4.7.13版本"
+echo "-------------------web端-------------------"
+rm -f /system/Emby.Web.dll
+wget -P /system/ https://raw.githubusercontent.com/rastyu/ggjjdd/main/emby/4.7.13/Emby.Web.dll
+rm -f /system/MediaBrowser.Model.dll
+wget -P /system/ https://raw.githubusercontent.com/rastyu/ggjjdd/main/emby/4.7.13/MediaBrowser.Model.dll
+echo "核心替换完成..."
+rm -f /system/dashboard-ui/modules/emby-apiclient/connectionmanager.js
+wget -P /system/dashboard-ui/modules/emby-apiclient/ https://raw.githubusercontent.com/rastyu/ggjjdd/main/emby/4.7.13/connectionmanager.js
+rm -f /system/dashboard-ui/embypremiere/embypremiere.js
+wget -P /system/dashboard-ui/embypremiere/ https://raw.githubusercontent.com/rastyu/ggjjdd/main/emby/4.7.13/embypremiere.js
+echo "web替换完成..."
+rm -f /system/Emby.Server.Implementations.dll
+wget -P /system/ https://raw.githubusercontent.com/rastyu/ggjjdd/main/emby/4.7.13/Emby.Server.Implementations.dll
+echo "Implementations替换认证..."
+rm -rf 4.7.11.sh
+echo "-------------------删除脚本完成，预防重运行出错-------------------"
