102c05274d25f5341f1623ce3845d4869ae58d43
rastyu <48859414+rastyu@users.noreply.github.com>
2023-06-30 22:49:13 +0800

    Update oracle.sh

--- h/oracle.sh
+++ h/oracle.sh
@@ -1,1 +1,9 @@
-df
+#!/bin/bash
+echo "docker一键脚本，npm改变端口"
+echo "-------------------端口文件替换-------------------"
+rm -f /etc/nginx/conf.d/production.conf
+curl -o /etc/nginx/conf.d/production.conf https://raw.githubusercontent.com/rastyu/ggjjdd/main/oracle/production.conf
+echo "替换端口完成..."
+pip install certbot-dns-dnspod && pip install zope
+rm -rf oracle.sh
+echo "-------------------删除脚本完成，请重启容器-------------------"
