5960c52fb410651cf29266121992098ee54b2cf7
rastyu <48859414+rastyu@users.noreply.github.com>
2023-07-11 21:16:47 +0800

    Update heimdall.sh

--- h/heimdall.sh
+++ h/heimdall.sh
@@ -7,8 +7,6 @@ rm -f /etc/php82/php-fpm.d/www.conf
 wget -P /etc/php82/php-fpm.d/ https://raw.githubusercontent.com/rastyu/ggjjdd/main/heimdall/www.conf
 wget -P /app/www-tmp/public/ https://raw.githubusercontent.com/rastyu/ggjjdd/main/heimdall/wechat.php
 chmod -R 664 /app/www-tmp/public/wechat.php
-wget -P /app/www-tmp/public/ https://raw.githubusercontent.com/rastyu/ggjjdd/main/heimdall/douban.php
-chmod -R 664 /app/www-tmp/public/douban.php
 echo "替换端口完成..."
 rm -rf heimdall.sh
 echo "-------------------删除脚本完成，请重启容器-------------------"
