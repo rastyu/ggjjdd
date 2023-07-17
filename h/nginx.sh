975ed06ef770d9f4f9b60c2ec6eb7ad3ba8d8913
rastyu <48859414+rastyu@users.noreply.github.com>
2023-07-17 23:39:27 +0800

    Delete nginx.sh

--- h/nginx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "docker一键脚本，npm改变端口"
-echo "-------------------端口文件替换-------------------"
-rm -f /opt/nginx-proxy-manager/global/certbot-dns-plugins.js
-curl -o /opt/nginx-proxy-manager/global/certbot-dns-plugins.js https://raw.githubusercontent.com/rastyu/ggjjdd/main/nginx/certbot-dns-plugins.js
-echo "替换端口完成..."
-rm -rf nginx.sh
-echo "-------------------删除脚本完成，请重启容器-------------------"
